20e423681a1730535898e69844cb3c279076ef74
afnleaf <afnleafow@gmail.com>
2026-02-26 09:09:20 -0500

    Added: macos native compilation option auto detect

--- build2.sh
+++ build2.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    PLATFORM_LIBS="-framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo"
+else
+    PLATFORM_LIBS="-lGL -lm -lpthread -ldl -lrt -lX11"
+fi
+
 if [ "$1" = "n" ]; then
-    gcc mecha.c -o mecha -I ./raylib/src -L ./lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+    gcc mecha.c -o mecha -I ./raylib/src -L ./lib -lraylib $PLATFORM_LIBS
 elif [ "$1" = "o" ]; then
-    gcc mecha.c -o mecha -O2 -march=native -flto -ffast-math -DNDEBUG -I ./raylib/src -L ./lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-    strip game
+    gcc mecha.c -o mecha -O2 -march=native -flto -ffast-math -DNDEBUG -I ./raylib/src -L ./lib -lraylib $PLATFORM_LIBS
+    strip mecha
 else
     emcc mecha.c -o mecha.js -Os -I ./raylib/src -L ./lib -l:libraylib.web.a -s USE_GLFW=3 -s SINGLE_FILE=1 -DPLATFORM_WEB
     histos config.yaml -o mecha.html
